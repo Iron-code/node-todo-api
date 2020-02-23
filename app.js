173c14b0c217509be1f3d6d64ae002e3f4b840a5
kenmartey <kenmartey89@gmail.com>
2020-02-23 11:44:10 -0300

    Move server to server.js and import in app.js

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
-var express = require('express')
-var app = express();
+const express = require('express')
+const app = express();
 
 app.get('/', function (req, res) {
     res.send("Hello World")
 })
 
-app.listen('8080', function () {
-    console.log('App is listening on port 8080')
-})
+module.exports = app 
