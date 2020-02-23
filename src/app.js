b83025889d961e065d95da2a9b268948e2e53129
lacasera <aboateng62@gmail.com>
2020-02-23 15:34:11 +0000

    Adds linting and Precommit hooks

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 const express = require('express')
-const app = express();
+const app = express()
 
 app.get('/', function (req, res) {
-    res.send("Hello World")
+     res.send('Hello World')
 })
 
-module.exports = app 
+module.exports = app
